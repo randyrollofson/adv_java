633974b0f4655924cb2a4d587f17126b72f7b28f
Randy Rollofson <randyrollofson@gmail.com>
2018-08-08 11:46:12 -0700

    adding phonebill-gwt

---
+++ phonebill-gwt/src/test/java/edu/pdx/cs410J/rr8/server/PhoneBillServiceImplTest.java
@@ -0,0 +1,17 @@
+package edu.pdx.cs410J.rr8.server;
+
+import edu.pdx.cs410J.rr8.client.PhoneBill;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PhoneBillServiceImplTest {
+
+  @Test
+  public void serviceReturnsExpectedPhoneBill() {
+    PhoneBillServiceImpl service = new PhoneBillServiceImpl();
+    PhoneBill bill = service.getPhoneBill();
+    assertThat(bill.getPhoneCalls().size(), equalTo(1));
+  }
+}
